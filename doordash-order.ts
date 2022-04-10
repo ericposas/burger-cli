85595fbfcf6b3d8ffbfec596e8181056252248d0
eric.posas <ericposas@outlook.com>
2022-04-10 12:32:48 -0500

    un-quoted js objects and types throughout

--- doordash-order.ts
+++ doordash-order.ts
@@ -26,84 +26,84 @@ const convertOrderValue = (total: number) => {
 	return total;
 };
 
-const completedOrderExample: CompletedOrderResponse = [
-    {
-        data: {
-            completedOrder: {
-                guid: "cc320a08-dfaf-43ab-8b53-ece2d36f03ae",
-                restaurant: {
-                    guid: '79eabbd6-b99d-4070-ad88-269906815528',
-                    name: 'Liberty Burger - Lakewood',
-                    whiteLabelName: 'Liberty Burger Lakewood',
-                    timeZoneId: 'America/Chicago',
-                    location: {
-                        address1: '1904 Abrams Parkway',
-                        address2: null,
-                        city: 'Dallas',
-                        state: 'TX',
-                        phone: '2148879999',
-                        __typename: 'Location'
-                    },
-                    creditCardConfig: {
-                        amexAccepted: true,
-                        tipEnabled: true,
-                        __typename: 'RestaurantCreditCardConfig'
-                    },
-                    loyaltyConfig: {
-                        loyaltyRedemptionEnabled: false,
-                        loyaltySignupEnabled: false,
-                        loyaltySignupBonus: null,
-                        __typename: 'LoyaltyConfig'
-                    },
-                    socialMediaLinks: {
-                        twitterLink: null,
-                        facebookLink: '',
-                        __typename: 'SocialMediaLinks'
-                    },
-                    __typename: 'Restaurant'
-                },
-                customerV2: null,
-                selections: [
-                    {
-                        modifiers: [],
-                        name: 'Baby Bella',
-                        price: 9,
-                        preDiscountPrice: 9,
-                        usesFractionalQuantity: false,
-                        fractionalQuantity: [Object],
-                        __typename: 'CompletedOrderSelection'
-                      }
-                ],
-                discounts: {
-                    restaurantDiscount: null,
-                    loyaltyDiscount: null,
-                    globalReward: null,
-                    __typename: 'Discounts'
-                },
-                discountsTotal: 0,
-                checkNumber: 74,
-                checkGuid: '64f3b9cd-f2b4-4471-912c-96452bebd076',
-                guestCommunication: 'When you come to pick up your ToGo order, please enter through the ToGo Door off the breezeway and proceed to the Togo Counter.',
-                deliveryChargeTotal: 0,
-                serviceChargeTotal: 0,
-                tfgRoundUpTotal: 0,
-                subtotal: 9,
-                tax: 0.74,
-                tip: 1.95,
-                total: 11.69,
-                estimatedFulfillmentDate: 1649526869413,
-                paymentType: 'CREDIT',
-                orderPaymentGuid: 'c97eed18-aa0c-4d76-b8b7-29eef60a31fe',
-                hasLoyaltyAttached: false,
-                curbsidePickup: { selected: false, __typename: 'CurbsidePickup' },
-                curbsidePickupV2: null,
-                giftCard: null,
-                deliveryInfo: null,
-                __typename: 'CompletedOrder'
-            }
-        }
-    }
-];
+// const completedOrderExample: CompletedOrderResponse = [
+//     {
+//         data: {
+//             completedOrder: {
+//                 guid: "cc320a08-dfaf-43ab-8b53-ece2d36f03ae",
+//                 restaurant: {
+//                     guid: '79eabbd6-b99d-4070-ad88-269906815528',
+//                     name: 'Liberty Burger - Lakewood',
+//                     whiteLabelName: 'Liberty Burger Lakewood',
+//                     timeZoneId: 'America/Chicago',
+//                     location: {
+//                         address1: '1904 Abrams Parkway',
+//                         address2: null,
+//                         city: 'Dallas',
+//                         state: 'TX',
+//                         phone: '2148879999',
+//                         __typename: 'Location'
+//                     },
+//                     creditCardConfig: {
+//                         amexAccepted: true,
+//                         tipEnabled: true,
+//                         __typename: 'RestaurantCreditCardConfig'
+//                     },
+//                     loyaltyConfig: {
+//                         loyaltyRedemptionEnabled: false,
+//                         loyaltySignupEnabled: false,
+//                         loyaltySignupBonus: null,
+//                         __typename: 'LoyaltyConfig'
+//                     },
+//                     socialMediaLinks: {
+//                         twitterLink: null,
+//                         facebookLink: '',
+//                         __typename: 'SocialMediaLinks'
+//                     },
+//                     __typename: 'Restaurant'
+//                 },
+//                 customerV2: null,
+//                 selections: [
+//                     {
+//                         modifiers: [],
+//                         name: 'Baby Bella',
+//                         price: 9,
+//                         preDiscountPrice: 9,
+//                         usesFractionalQuantity: false,
+//                         fractionalQuantity: [Object],
+//                         __typename: 'CompletedOrderSelection'
+//                       }
+//                 ],
+//                 discounts: {
+//                     restaurantDiscount: null,
+//                     loyaltyDiscount: null,
+//                     globalReward: null,
+//                     __typename: 'Discounts'
+//                 },
+//                 discountsTotal: 0,
+//                 checkNumber: 74,
+//                 checkGuid: '64f3b9cd-f2b4-4471-912c-96452bebd076',
+//                 guestCommunication: 'When you come to pick up your ToGo order, please enter through the ToGo Door off the breezeway and proceed to the Togo Counter.',
+//                 deliveryChargeTotal: 0,
+//                 serviceChargeTotal: 0,
+//                 tfgRoundUpTotal: 0,
+//                 subtotal: 9,
+//                 tax: 0.74,
+//                 tip: 1.95,
+//                 total: 11.69,
+//                 estimatedFulfillmentDate: 1649526869413,
+//                 paymentType: 'CREDIT',
+//                 orderPaymentGuid: 'c97eed18-aa0c-4d76-b8b7-29eef60a31fe',
+//                 hasLoyaltyAttached: false,
+//                 curbsidePickup: { selected: false, __typename: 'CurbsidePickup' },
+//                 curbsidePickupV2: null,
+//                 giftCard: null,
+//                 deliveryInfo: null,
+//                 __typename: 'CompletedOrder'
+//             }
+//         }
+//     }
+// ];
 
 type DeliveryQuoteBody = {
     external_delivery_id: string,
@@ -136,7 +136,7 @@ type DeliveryQuoteBody = {
     }
 };
 
-export type DeliveryQuoteResponse = {
+type DeliveryQuoteResponse = {
     external_delivery_id: string,
     currency: string | 'USD',
     delivery_status: string | 'quote',
@@ -175,19 +175,6 @@ type CreateDeliveryBody = {
 	order_value: number
 };
 
-const createDeliveryBody = JSON.stringify(<CreateDeliveryBody>{
-	external_delivery_id: uuidv4(),
-	pickup_address: completedOrderExample[0].data.completedOrder.restaurant.location.address1,
-	pickup_business_name: completedOrderExample[0].data.completedOrder.restaurant.name,
-	pickup_phone_number: completedOrderExample[0].data.completedOrder.restaurant.location.phone,
-	pickup_instructions: completedOrderExample[0].data.completedOrder.guestCommunication,
-	dropoff_address: process.env.doordash_address,
-	dropoff_business_name: `${process.env.dropoff_business_name}`,
-	dropoff_phone_number: `+1${process.env.phone}`,
-	dropoff_instructions: "Ring doorbell and/or text please!",
-	order_value: convertOrderValue(completedOrderExample[0].data.completedOrder.total)
-});
-
 type CreateDeliveryResponse = {
 	external_delivery_id: string, 
 	currency: string | 'USD',
@@ -216,7 +203,7 @@ const getDeliveryQuote = async (liberty_burger_completed_order_id: string): Prom
         const completedOrder: CompletedOrderResponse = await getCompletedOrderInfo(liberty_burger_completed_order_id);
         console.log(
             completedOrder
-        )
+        );
         // package it in the format DD expects
         const body = JSON.stringify(<DeliveryQuoteBody>{
             external_delivery_id: uuidv4(),
@@ -257,9 +244,28 @@ const getDeliveryQuote = async (liberty_burger_completed_order_id: string): Prom
     }
 };
 
-const createDelivery = async (): Promise<CreateDeliveryResponse> => {
-	try {
-		const result: AxiosResponse<CreateDeliveryResponse> = await axios.post('https://openapi.doordash.com/drive/v2/deliveries', createDeliveryBody, getHeaders());
+const createDelivery = async (liberty_burger_completed_order_id: string): Promise<CreateDeliveryResponse> => {
+    try {
+        // get completed order data from Liberty
+        const completedOrder: CompletedOrderResponse = await getCompletedOrderInfo(liberty_burger_completed_order_id);
+        console.log(
+            completedOrder
+        );
+        // prepare body to send to DD
+        const body = JSON.stringify(<CreateDeliveryBody>{
+            external_delivery_id: uuidv4(),
+            pickup_address: completedOrder[0].data.completedOrder.restaurant.location.address1,
+            pickup_business_name: completedOrder[0].data.completedOrder.restaurant.name,
+            pickup_phone_number: completedOrder[0].data.completedOrder.restaurant.location.phone,
+            pickup_instructions: completedOrder[0].data.completedOrder.guestCommunication,
+            dropoff_address: process.env.doordash_address,
+            dropoff_business_name: `${process.env.dropoff_business_name}`,
+            dropoff_phone_number: `+1${process.env.phone}`,
+            dropoff_instructions: "Ring doorbell and/or text please!",
+            order_value: convertOrderValue(completedOrder[0].data.completedOrder.total)
+        });
+        // send body to DD create delivery endpoint 
+		const result: AxiosResponse<CreateDeliveryResponse> = await axios.post('https://openapi.doordash.com/drive/v2/deliveries', body, getHeaders());
 		return Promise.resolve(result.data);
 	} catch (err) {
 		console.error(err);
@@ -282,36 +288,39 @@ const getDeliveryStatus = async (external_delivery_id: string) => {
 
 	// read cli commands
 	if (command) {
-        if (command === 'quote') {
-            if (arg2 && typeof arg2 === 'string') {
+
+        if (arg2 && typeof arg2 === 'string') {
+            if (command === 'quote') {
                 const result = await getDeliveryQuote(arg2);
                 console.log('quoted fee: ', result);
-            } else {
-                console.log(
-                    'you need to pass in a completed order id from Liberty Burger!'
-                );
             }
+            if (command === 'status') {
+                const result = await getDeliveryStatus(arg2);
+                console.log(result);
+            }
+            if (command === 'create') {
+                const result = await createDelivery(arg2);
+                console.log(result);
+            }
+        } else {
+            console.log(
+                'you need to pass in a completed order id from Liberty Burger!'
+            );
         }
-		if (command === 'status' && arg2 && typeof arg2 === 'string') {
-			const result = await getDeliveryStatus(arg2);
-			console.log(result);
-		}
-		if (command === 'create') {
-			const result = await createDelivery();
-			console.log(result);
-		}
+
 	} else {
-		console.log('provide a console command: "create" -- creates a delivery (will need to pass in order details from Liberty Burger) or "status" w/a passing in external_delivery_id string checks status of a delivery')
+		console.log('provide a console command: "quote", "create", or "status"')
 	}
+    
     if (command !== 'quote' && command !== 'status' && command !== 'create') {
         console.log('invalid command!');
     }
+
 })();
 
-// getDeliveryQuote();
-
+// exports
 module.exports = {
     getHeaders,
-    completedOrderExample,
+    // completedOrderExample,
     convertOrderValue
 };
